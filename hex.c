b2e70b3ea74453bee78cc0c53dd49fc4a843c5c2
RachnaKedigehalli <54764594+RachnaKedigehalli@users.noreply.github.com>
2019-10-29 14:29:34 +0530

    Update hex.c

--- hex.c
+++ hex.c
@@ -1,30 +1,33 @@
+//INPUT  : 2 integers, 'a' and 'base'.
+//OUTPUT : Hexadecimal form of 'a' for the base 'base'
 #include <stdio.h>
 #include<stdlib.h>
-int count=0;					
+int count=0;						//Global variable
 
-char hex(int a, char *output)	
+char hex(int a, int base, char *output)			//Recursion: Stores the output in the reverse order
 {
 	if(a>0)
 	{
-		if(a%16<10)
-			*(output+count) = '0'+a%16;
+		if(a%base<10)
+			*(output+count) = '0'+a%base;	//using ASCII value to store the character
 		else
-			*(output+count) = 'a'+a%16-10;
+			*(output+count) = 'a'+a%base-10;//using ASCII value to store the character
 		count++;
-		return hex(a/16, output);
+		return hex(a/base, base, output);
 	}
 }
 
 int main()
 {
-	int a;
-	scanf("%d", &a);	
+	int a, base;
+	scanf("%d", &a);				//INPUT
+	scanf("%d", &base);
 	char *output;
-	output = (char*)malloc(31*sizeof(char));
-	hex(a, output);	
-	for (int j=count; j>0; j--)	
+	output = (char*)malloc(31*sizeof(char));	//Initialisation of the pointer
+	hex(a, base, output);				//Calling the function
+	for (int j=count; j>0; j--)			//OUTPUT
 		printf("%c", *(output+j-1));
 	printf("\n");
-	free(output);
+	free(output);					//De-allocation
 	return 0;
 }
